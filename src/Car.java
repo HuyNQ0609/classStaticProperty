c865073adb6b4af9371f20af8182ecce85cb7070
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-01-11 08:34:14 +0700

    Static Property

--- src/Car.java
+++ src/Car.java
@@ -2,30 +2,24 @@ public class Car {
     private String name;
     private String engine;
     public static int numberOfCars;
-
     public Car(String name, String engine) {
         this.name = name;
         this. engine = engine;
         numberOfCars++;
     }
     //*-----getter and setter-----*//
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEngine() {
         return engine;
     }
-
     public void setEngine(String engine) {
         this.engine = engine;
     }
-
     @Override
     public String toString() {
         return "Car{" +
